a565872daef82ea501cf745100d706effaef1692
Paul K Moore <paul@whosebill.com>
2012-02-06 23:51:47 +0000

    Ported RKManagedObjectStore:findOrCreateInstanceOfEntity to use MagicalRecord.

--- Code/CoreData/RKManagedObjectStore+MagicalRecord.h
+++ Code/CoreData/RKManagedObjectStore+MagicalRecord.h
@@ -10,12 +10,34 @@
 
 @interface RKManagedObjectStore (MagicalRecord)
 
+/**
+ * Overload of the designated initializer in order to use MagicalRecord.
+ *
+ * @todo {This implementation does NOT support i) specification of the store directory, 
+ * ii) managed object model or iii) seed database.  This will cause related functionality 
+ * in RestKit not to work, and is a work-in-progress.}
+ */
 - (id)initWithStoreFilename:(NSString *)storeFilename
                 inDirectory:(NSString *)nilOrDirectoryPath
       usingSeedDatabaseName:(NSString *)nilOrNameOfSeedDatabaseInMainBundle
          managedObjectModel:(NSManagedObjectModel*)nilOrManagedObjectModel
                    delegate:(id)delegate;
 
+/**
+ * We overload the pathToStoreFile method here in order that the filepath is 
+ * retrieved from the relevant NSPersistentStore, rather than a retained
+ * attribute of the RKObjectStore.
+ */
 - (NSString*)pathToStoreFile;
+
+/**
+ *	Overload managedObjectContext getter to ensure we return a separate context
+ *	for each NSThread (via MagicalRecord).
+ */
+-(NSManagedObjectContext*)managedObjectContext;
+
+- (NSManagedObject*)findOrCreateInstanceOfEntity:(NSEntityDescription*)entity
+                         withPrimaryKeyAttribute:(NSString*)primaryKeyAttribute
+                                        andValue:(id)primaryKeyValue;
 
 @end
--- Code/CoreData/RKManagedObjectStore+MagicalRecord.m
+++ Code/CoreData/RKManagedObjectStore+MagicalRecord.m
@@ -11,7 +11,10 @@
 #import "CoreData+MagicalRecord.h"
 
 @implementation RKManagedObjectStore (MagicalRecord)
-  
+
+#pragma mark -
+#pragma mark Initialization Methods
+
 - (id)initWithStoreFilename:(NSString *)storeFilename inDirectory:(NSString *)nilOrDirectoryPath usingSeedDatabaseName:(NSString *)nilOrNameOfSeedDatabaseInMainBundle managedObjectModel:(NSManagedObjectModel*)nilOrManagedObjectModel delegate:(id)delegate {
     self = [self init];
     if (self) {
@@ -27,13 +30,38 @@
     return self;
 }
 
-/**
- * We overload the pathToStoreFile method here in order that the filepath is 
- * retrieved from the relevant NSPersistentStore, rather than a retained
- * attribute of the RKObjectStore.
- */
+#pragma mark -
+#pragma mark Custom Accessors
+
 - (NSString*)pathToStoreFile {
     return [[NSPersistentStore urlForStoreName:[self storeFilename]] path];
 }
+
+- (NSManagedObjectContext*)managedObjectContext {
+	return [NSManagedObjectContext contextForCurrentThread];
+}
+
+#pragma mark -
+#pragma mark Helper Methods
+
+- (NSManagedObject*)findOrCreateInstanceOfEntity:(NSEntityDescription*)entity withPrimaryKeyAttribute:(NSString*)primaryKeyAttribute andValue:(id)primaryKeyValue {
+    NSAssert(entity, @"Cannot instantiate managed object without a target class");
+    NSAssert(primaryKeyAttribute, @"Cannot find existing managed object instance without a primary key attribute");
+    NSAssert(primaryKeyValue, @"Cannot find existing managed object by primary key without a value");
+	NSManagedObject* object = nil;
+   
+    // Try to find the object
+    object = [NSClassFromString([entity managedObjectClassName]) findFirstByAttribute:(NSString *)primaryKeyAttribute
+                                                                            withValue:(id)primaryKeyValue];  // Note: the MagicalRecord method is a class method, and requires us to 'obtain' the specific class for the requested entity.
+
+    // If we can't find the object, create a new one...
+    if (!object) {
+        object = [[[NSManagedObject alloc] initWithEntity:entity insertIntoManagedObjectContext:[NSManagedObjectContext contextForCurrentThread]] autorelease];
+    }
+    
+	return object;
+}
+
+
 
 @end
